caac9797068ec8758b11573dee78d71826eb2d63
Peter Dalmaris <peter@futureshock.com.au>
2016-08-02 07:11:36 +1000

    Update split_recording.py

--- split_recording.py
+++ split_recording.py
@@ -1,10 +1,14 @@
-from picamera import PiCamera
+# This script shows how to record video in multiple small files
+# instead of a single large one.
+# Written by Peter Dalmaris for the course Raspberry Pi: Create a Bench Computer
 
-cam = PiCamera()
-cam.start_recording('1.h264')
-cam.wait_recording(5)
-for i in range(2,5):
-  cam.split_recording('%d.h264' % i)
-  cam.wait_recording(5)
+from picamera import PiCamera             # Import the require module
 
-cam.stop_recording()
+cam = PiCamera()                          # Create the PiCamera object
+cam.start_recording('1.h264')             # Start recording video...
+cam.wait_recording(5)                     # ... for 5 seconds
+for i in range(2,5):                      # ... then create a loop that will create 4 files
+  cam.split_recording('%d.h264' % i)      # each file will have a unique name,
+  cam.wait_recording(5)                   # and contain 5 seconds of video
+
+cam.stop_recording()                      # stop the video recording
